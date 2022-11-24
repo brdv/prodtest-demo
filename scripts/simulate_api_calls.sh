58f06a999ccc9a12d46d49205b8a623e6472451c
brdv <brdv@pm.me>
2022-11-24 12:15:58 +0100

    add make dir if not exists

--- scripts/simulate_api_calls.sh
+++ scripts/simulate_api_calls.sh
@@ -1,24 +1,32 @@
 #!/bin/bash
-
 help() {
     echo "Use this script as follows:"
     echo "  sh path/to/simulate_api_calls.sh [options]\n"
     echo "Options are as follows:"
     echo "  -n: number -> number of times the api should be called.\n"
     echo "  -h: bool -> call for the help message.\n"
-    exit {0}
+    exit 0
 }
 
 call() {
     curl --request POST -H "Content-Type:application/json" http://localhost/orders --data "{\"ItemIds\": [\"burger\", \"fries\"]}" --silent 
 }
 
-NO_CALLS=1000
-RESPONSE_FILE="temp/request_output.json"
+NO_CALLS=500
+RESPONSE_DIR="temp"
+RESPONSE_FILE="request_output.json"
+RESPONSE_FILE_PATH="$RESPONSE_DIR/$RESPONSE_FILE"
 
-if [ -f $RESPONSE_FILE ]; then
-   rm $RESPONSE_FILE
-   echo "File: $RESPONSE_FILE is removed"
+echo $RESPONSE_FILE_PATH
+
+if [ ! -d "$RESPONSE_DIR" ];
+then
+	mkdir $RESPONSE_DIR
+fi
+
+if [ -f $RESPONSE_FILE_PATH ]; then
+   rm $RESPONSE_FILE_PATH
+   echo "File: $RESPONSE_FILE_PATH is removed"
 fi
 
 
@@ -49,20 +57,21 @@ echo "API will be called $NO_CALLS times"
 
 COUNT=0
 
-echo "[" >> $RESPONSE_FILE
+echo "[" >> $RESPONSE_FILE_PATH
 
 # call api NO_CALLS times and append response to RESPONSE_FILE
 while test $COUNT -lt $NO_CALLS; do
     # function as defined above.
     output=$(call)
 
-    echo "${output}" | jq >> $RESPONSE_FILE
-    ((COUNT=COUNT+1))
+    echo "${output}" | jq >> $RESPONSE_FILE_PATH
+    COUNT=$((COUNT+1))
+
     if test $COUNT -lt $NO_CALLS; then
-        echo "," >> $RESPONSE_FILE
+        echo "," >> $RESPONSE_FILE_PATH
     fi
 done
 
-echo "]" >> $RESPONSE_FILE
+echo "]" >> $RESPONSE_FILE_PATH
 
-echo "See '$RESPONSE_FILE' for results"
+echo "See '$RESPONSE_FILE_PATH' for results"
