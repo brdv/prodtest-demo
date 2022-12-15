8a6067aa873de5dafd6b8260ccc6af5260c0361a
brdv <brdv@pm.me>
2022-12-15 12:19:26 +0100

    add rabbit mock

--- src/Services/Order/Order.API.Tests/Unit/OrderService.Test.cs
+++ src/Services/Order/Order.API.Tests/Unit/OrderService.Test.cs
@@ -1,4 +1,6 @@
-﻿using Order.API.Services;
+﻿using Domain.Common.Services;
+using Moq;
+using Order.API.Services;
 using Order.API.Tests.TestData;
 
 namespace Order.API.Tests.Unit;
@@ -6,11 +8,26 @@ namespace Order.API.Tests.Unit;
 [TestClass]
 public class OrderServiceTests
 {
-    private readonly IOrderService _service = new OrderService();
+    [AssemblyInitialize]
+    public static void AssemblyInit(TestContext context)
+    {
+        Environment.SetEnvironmentVariable("DL_TAG_VERSION", "Vtest");
+        Environment.SetEnvironmentVariable("RMQ_HOST", "testhost");
+        Environment.SetEnvironmentVariable("DOTNET_ENVIRONMENT", "test");
+
+    }
+
     [TestMethod]
     public void GetMenuItems_Returns_Default_List()
     {
-        var result = _service.GetMenuItems();
+        var mock = new Mock<IRabbitMQService>(MockBehavior.Strict);
+
+        mock.Setup(service => service.PublishEvent(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>())).Verifiable();
+        var mockService = mock.Object;
+
+        var service = new OrderService(mockService);
+
+        var result = service.GetMenuItems();
 
         Assert.IsNotNull(result);
         Assert.AreNotEqual(0, result.Count);
@@ -26,10 +43,15 @@ public class OrderServiceTests
     public void AddOrder_Maps_Items_Correctly()
     {
         // arrange
+        var mock = new Mock<IRabbitMQService>(MockBehavior.Strict);
+
+        mock.Setup(service => service.PublishEvent(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>())).Verifiable();
+        var mockService = mock.Object;
         var postData = OrderTestData.GetTestOrderCreate();
+        var service = new OrderService(mockService);
 
         // act
-        var result = _service.AddOrder(postData);
+        var result = service.AddOrder(postData);
 
         // assert
         Assert.IsNotNull(result);
