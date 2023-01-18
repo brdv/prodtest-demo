d14f5418cae186269a1274227bc255e75664447e
brdv <brdv@pm.me>
2023-01-18 20:04:40 +0100

    add cors

--- src/Services/Order/Order.API/Program.cs
+++ src/Services/Order/Order.API/Program.cs
@@ -1,18 +1,38 @@
-﻿// Stryker disable all : Program.cs not tested
+﻿
+
+// Stryker disable all : Program.cs not tested
 using Domain.Common.Services;
 using Order.API.Services;
+
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddScoped<IOrderService, OrderService>()
                 .AddScoped<IRabbitMQService, RabbitMQService>();
 builder.Services.AddControllers();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(MyAllowSpecificOrigins,
+                          policy =>
+                          {
+                              policy.AllowAnyOrigin()
+                                                  .AllowAnyHeader()
+                                                  .AllowAnyMethod();
+                          });
+});
+
 
 var prodtestEnv = Environment.GetEnvironmentVariable("PRODTEST_VERSION");
 var env = prodtestEnv != null ? prodtestEnv : "development";
 
 builder.Configuration.AddJsonFile(env == "latest" ? "appsettings.json" : $"appsettings.{env}.json");
 
+
 var app = builder.Build();
+
+app.UseCors(MyAllowSpecificOrigins);
+
 
 app.UseHttpsRedirection();
 app.MapControllers();
